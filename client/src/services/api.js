df24a827317a9df6abe15a2a596160c060954fd8
vishal <vishalkrishna205@gmail.com>
2026-07-02 20:08:01 +0530

    Conect frontend to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://studyhub-9kpn.onrender.com/api",
 });
 
 // Attach JWT token to every request if available
